953fd8806c28b835ec5a1a690fa513dedec8c7ca
Connection <69660530+ConnectionOuOb@users.noreply.github.com>
2024-08-28 20:32:24 +0800

    Update NTHU-Curriculum

--- NTHU-Curriculum/flutter_service_worker.js
+++ NTHU-Curriculum/flutter_service_worker.js
@@ -39,9 +39,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "fa53185c53640c6e0268754e26d624cc
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "4ad30fd8bbb0e8d9d4a46f138997028e",
-"/": "4ad30fd8bbb0e8d9d4a46f138997028e",
-"main.dart.js": "c0f7942a6d645d3b35d4496d9b41d1fc",
+"index.html": "584182b6b7aa942e1aaab0e59373d93d",
+"/": "584182b6b7aa942e1aaab0e59373d93d",
+"main.dart.js": "60d32a01f7563a091dd67f747d6f9423",
 "manifest.json": "4de9fa27ebf36c33eb34f932ce99caa2",
 "version.json": "45b27ea2a0d7afbb494963c66bb4d279"};
 // The application shell files that are downloaded before a service worker can
